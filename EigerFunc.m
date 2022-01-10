f21795c2844721950ffa584a743de383831546d9
skull <skull@172.18.36.32>
2022-01-10 10:23:12 +0800

    add read master info and data

--- EigerFunc.m
+++ EigerFunc.m
@@ -2,5 +2,9 @@ classdef EigerFunc
     methods (Static = true)
         Ver = Version();
         EnvConfig = EnvConfiguration();
+        
+        MasterInfo =ReadMaster(MasterFP);
+        SingleFrameData = ReadData(MasterInfo,RequestSN,XRange,YRange);
+        
     end
 end
